27c6bb1517c67742770df0defadad43acbca1ded
kevin <1030397448@qq.com>
2019-07-23 19:49:49 +0800

    add image widget flutter project

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # Flutter-Learning
 Demo code for learning Flutter in my blog: https://zhengyanfeng.github.io
 
-1.flutter_app_helloworld: https://zhengyanfeng.github.io/post/flutter-xue-xi-udacity-ke-cheng/
+1. flutter_app_helloworld: https://zhengyanfeng.github.io/post/flutter-xue-xi-udacity-ke-cheng/
 
-2.flutter_app_textwidget: https://zhengyanfeng.github.io/post/flutter-text-widget-wen-ben-zu-jian-de-shi-yong/
+2. flutter_app_textwidget: https://zhengyanfeng.github.io/post/flutter-text-widget-wen-ben-zu-jian-de-shi-yong/
+
+3. flutter_app_container: 
---
+++ flutter_app_image/lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+
+      home: Scaffold(
+        body: Center(
+          child: Container(
+            child: new Image.network(
+              'https://flutter-io.cn/favicon.ico',
+              scale: 1.0,
+              color: Colors.greenAccent,
+              colorBlendMode: BlendMode.colorBurn,
+
+              repeat: ImageRepeat.repeat,
+            ),
+
+            width: 600,
+            height: 900,
+            color: Colors.lightBlue,
+          ), //child
+        ), //body
+      ), //home
+    ); //MaterialApp
+  }
+}
